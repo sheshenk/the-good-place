fd89e5d577b3924b5db2ea8107ae7afb42db25ae
Shashank Acharya <acharya.s@icloud.com>
2022-03-12 00:01:27 +0800

    added charts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import '@fontsource/roboto/700.css';
 import { AppBar, CssBaseline, Divider, Drawer, ListItem, ListItemIcon, ListItemText, Tab, Tabs, Toolbar, Typography, List, Button } from '@mui/material';
 import { Box } from '@mui/system';
 import AppDrawer from './components/AppDrawer/AppDrawer';
+import AppDashboard from './pages/AppDashboard/AppDashboard';
+import { Dashboard } from '@mui/icons-material';
+import { MainContainer } from './components/MainContainer/MainContainer';
 
 const drawerWidth = 220
 
@@ -13,29 +16,10 @@ function App() {
   return (
     <Box sx={{display: `flex`}}>
       <CssBaseline/>
-      <AppBar sx={{
-        width: `calc(100% - ${drawerWidth}px)`, 
-        ml: `${drawerWidth}px`, 
-        boxShadow:'none', 
-        bgcolor:'rgba(255,255,255,0.8)',
-        backdropFilter: 'blur(20px)',
-        color: '#333'
-      }}>
-        <Toolbar>
-          <Typography variant='h5'>Dashboard</Typography>
-        </Toolbar>
-        <Divider/>
-      </AppBar>
       <AppDrawer dwidth = {drawerWidth}/>
-      <Box
-        component='main'
-        sx={{flexGrow:1, bgcolor:'Background.default',p:3}}
-      >
-        <Toolbar/>
-        <Typography paragraph>
-          Hi
-        </Typography>
-      </Box>
+      <MainContainer title='Dashboard' drawerWidth={drawerWidth}>
+        <AppDashboard/>
+      </MainContainer>
     </Box>
   );
 }
---
+++ src/components/MainContainer/MainContainer.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { AppBar, CssBaseline, Divider, Drawer, ListItem, ListItemIcon, ListItemText, Tab, Tabs, Toolbar, Typography, List, Button } from '@mui/material';
+import { Box } from '@mui/system';
+
+export const MainContainer = ({title, drawerWidth, children }) => {
+    return (
+        <div>
+            <AppBar sx={{
+                width: `calc(100% - ${drawerWidth}px)`,
+                ml: `${drawerWidth}px`,
+                boxShadow: 'none',
+                bgcolor: 'rgba(255,255,255,0.6)',
+                backdropFilter: 'blur(20px)',
+                color: '#333'
+            }}>
+                <Toolbar>
+                    <Typography variant='h5'>{title}</Typography>
+                </Toolbar>
+                <Divider />
+            </AppBar>
+            <Box
+                component='main'
+                sx={{ flexGrow: 1, bgcolor: 'Background.default', p: 3 }}
+            >
+                <Toolbar />
+                {children}
+            </Box>
+        </div>
+    )
+}
